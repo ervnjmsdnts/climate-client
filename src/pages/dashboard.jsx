b13d58a5a54ec6562c50c5d1ab83dd672450b9c0
ervnjmsdnts <earvinjamesdantes@gmail.com>
2023-10-13 20:35:35 +0800

    added log out button

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,3 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import { supabaseClient } from '../lib/supabase';
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+  const logout = async () => {
+    const { error } = await supabaseClient.auth.signOut();
+    if (error) throw error;
+
+    navigate('/auth');
+  };
+
+  return (
+    <div>
+      Dashboard
+      <button className='btn btn-primary' onClick={logout}>
+        Sign out
+      </button>
+    </div>
+  );
 }
