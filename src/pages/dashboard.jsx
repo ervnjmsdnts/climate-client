3d008bbf01f1a9711f688012af1800dc76d7f88b
ervnjmsdnts <earvinjamesdantes@gmail.com>
2023-11-05 20:30:52 +0800

    Added filtering and CSV

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,6 +1,14 @@
 import { supabaseClient } from '../lib/supabase';
 import { useQuery, useQueryClient } from 'react-query';
-import { format } from 'date-fns';
+import {
+  endOfWeek,
+  format,
+  isSameDay,
+  isSameMonth,
+  isWithinInterval,
+  parseISO,
+  startOfWeek,
+} from 'date-fns';
 import { useEffect } from 'react';
 import { Thermometer } from 'lucide-react';
 import { Droplet } from 'lucide-react';
@@ -13,9 +21,14 @@ import {
   Tooltip,
   YAxis,
 } from 'recharts';
+import { useState } from 'react';
+import { Loader2 } from 'lucide-react';
+import { useMemo } from 'react';
+import { CSVLink } from 'react-csv';
 
 function CustomTooltip({ active, payload, type }) {
   if (active && payload && payload.length) {
+    console.log(payload[0].payload);
     return (
       <div
         className={`p-2 rounded-lg font-semibold text-white ${
@@ -26,6 +39,7 @@ function CustomTooltip({ active, payload, type }) {
             ? `${payload[0].value}\xB0C`
             : `${payload[0].value}%`}
         </p>
+        <p>{format(new Date(payload[0].payload.created_at), 'PPp')}</p>
       </div>
     );
   }
@@ -34,16 +48,67 @@ function CustomTooltip({ active, payload, type }) {
 }
 
 export default function Dashboard() {
-  const { data: climates } = useQuery({
+  const { data: climates, isLoading } = useQuery({
     queryKey: 'climates',
     queryFn: async () => {
       const { data } = await supabaseClient
         .from('climate')
         .select()
-        .order('created_at', { ascending: false });
+        .order('created_at', { ascending: true });
       return data;
     },
   });
+
+  const [selectedFilter, setSelectedFilter] = useState('All Time');
+
+  const filteredData = useMemo(() => {
+    let filteredData = climates;
+
+    if (selectedFilter === 'Current Day') {
+      const currentDate = new Date();
+      filteredData = filteredData.filter((climate) =>
+        isSameDay(parseISO(climate.created_at), currentDate),
+      );
+    } else if (selectedFilter === 'Current Month') {
+      const currentDate = new Date();
+      filteredData = filteredData.filter((climate) =>
+        isSameMonth(parseISO(climate.created_at), currentDate),
+      );
+    } else if (selectedFilter === 'Current Week') {
+      const currentDate = new Date();
+      const weekStart = startOfWeek(currentDate);
+      const weekEnd = endOfWeek(currentDate);
+
+      filteredData = filteredData.filter((climate) =>
+        isWithinInterval(parseISO(climate.created_at), {
+          start: weekStart,
+          end: weekEnd,
+        }),
+      );
+    }
+
+    filteredData?.sort(
+      (a, b) =>
+        new Date(a.created_at).getTime() - new Date(b.created_at).getTime(),
+    );
+
+    return filteredData;
+  }, [climates, selectedFilter]);
+
+  const sortedDataForTable = filteredData
+    ?.slice()
+    .sort(
+      (a, b) =>
+        new Date(b.created_at).getTime() - new Date(a.created_at).getTime(),
+    );
+
+  const csvData = sortedDataForTable?.map((data) => ({
+    Temperature: data.temperature,
+    Humidity: data.humidity,
+    Date: format(new Date(data.created_at), 'PPP').toString(),
+  }));
+
+  const latestClimate = sortedDataForTable?.[0];
 
   const queryClient = useQueryClient();
 
@@ -70,95 +135,121 @@ export default function Dashboard() {
 
   return (
     <div className='p-8 flex-grow'>
-      <div className='grid grid-cols-6 grid-rows-4 gap-4 h-full'>
-        <div className='col-span-2 flex gap-4'>
-          <div className='card bg-red-300 w-full text-white hover:shadow-xl shadow-lg'>
-            <div className='card-body'>
-              <div className='flex justify-between items-center'>
-                <h2 className='card-title'>Temperature</h2>
-                <Thermometer />
+      {climates && climates.length > 0 ? (
+        <div className='grid grid-cols-6 grid-rows-4 gap-4 h-full'>
+          <div className='col-span-2 flex gap-4'>
+            <div className='card bg-red-300 w-full text-white hover:shadow-xl shadow-lg'>
+              <div className='card-body'>
+                <div className='flex justify-between items-center'>
+                  <h2 className='card-title'>Temperature</h2>
+                  <Thermometer />
+                </div>
+                <div className='flex items-center justify-center h-full'>
+                  <h2 className='text-4xl text-center font-semibold'>
+                    {latestClimate?.temperature}&deg;C
+                  </h2>
+                </div>
               </div>
-              <div className='flex items-center justify-center h-full'>
-                <h2 className='text-4xl text-center font-semibold'>20&deg;C</h2>
+            </div>
+            <div className='card bg-blue-300 w-full text-white hover:shadow-xl shadow-lg'>
+              <div className='card-body'>
+                <div className='flex justify-between items-center'>
+                  <h2 className='card-title'>Humidity</h2>
+                  <Droplet />
+                </div>
+                <div className='flex items-center justify-center h-full'>
+                  <h2 className='text-4xl text-center font-semibold'>
+                    {latestClimate.humidity}%
+                  </h2>
+                </div>
               </div>
             </div>
           </div>
-          <div className='card bg-blue-300 w-full text-white hover:shadow-xl shadow-lg'>
-            <div className='card-body'>
-              <div className='flex justify-between items-center'>
-                <h2 className='card-title'>Humidity</h2>
-                <Droplet />
+          <div className='col-span-4 row-span-4 gap-4 flex flex-col'>
+            <div className='self-end'>
+              <select
+                onChange={(e) => setSelectedFilter(e.target.value)}
+                className='select select-bordered rounded-lg w-full max-w-xs'>
+                <option value='All Time'>All Time</option>
+                <option value='Current Day'>Current Day</option>
+                <option value='Current Week'>Current Week</option>
+                <option value='Current Month'>Current Month</option>
+              </select>
+            </div>
+            <div className='card h-full shadow-lg hover:shadow-xl p-4'>
+              <div className='card-body'>
+                <ResponsiveContainer width='100%' height='100%'>
+                  <LineChart data={filteredData}>
+                    <CartesianGrid strokeDasharray='3 3' />
+                    <YAxis />
+                    <Legend />
+                    <Tooltip content={<CustomTooltip type='temp' />} />
+                    <Line
+                      type='monotone'
+                      dataKey='temperature'
+                      stroke='#fca5a5'
+                    />
+                  </LineChart>
+                </ResponsiveContainer>
               </div>
-              <div className='flex items-center justify-center h-full'>
-                <h2 className='text-4xl text-center font-semibold'>30%</h2>
+            </div>
+            <div className='card h-full shadow-lg hover:shadow-xl p-4'>
+              <div className='card-body'>
+                <ResponsiveContainer width='100%' height='100%'>
+                  <LineChart data={filteredData}>
+                    <CartesianGrid strokeDasharray='3 3' />
+                    <Tooltip content={<CustomTooltip type='hum' />} />
+                    <YAxis />
+                    <Legend />
+                    <Line type='monotone' dataKey='humidity' stroke='#93c5fd' />
+                  </LineChart>
+                </ResponsiveContainer>
               </div>
             </div>
           </div>
-        </div>
-        <div className='col-span-4 row-span-4 gap-4 flex flex-col'>
-          <div className='card h-full shadow-lg hover:shadow-xl p-4'>
-            <div className='card-body'>
-              <ResponsiveContainer width='100%' height='100%'>
-                <LineChart data={climates}>
-                  <CartesianGrid strokeDasharray='3 3' />
-                  <YAxis />
-                  <Legend />
-                  <Tooltip content={<CustomTooltip type='temp' />} />
-                  <Line
-                    type='monotone'
-                    dataKey='temperature'
-                    stroke='#fca5a5'
-                  />
-                </LineChart>
-              </ResponsiveContainer>
-            </div>
-          </div>
-          <div className='card h-full shadow-lg hover:shadow-xl p-4'>
-            <div className='card-body'>
-              <ResponsiveContainer width='100%' height='100%'>
-                <LineChart data={climates}>
-                  <CartesianGrid strokeDasharray='3 3' />
-                  <Tooltip content={<CustomTooltip type='hum' />} />
-                  <YAxis />
-                  <Legend />
-                  <Line type='monotone' dataKey='humidity' stroke='#93c5fd' />
-                </LineChart>
-              </ResponsiveContainer>
-            </div>
-          </div>
-        </div>
-        <div className='card shadow-lg col-span-2 flex flex-col row-span-3'>
-          <div className='flex-grow card-body h-0 overflow-y-auto'>
-            <table className='table'>
-              <thead>
-                <tr>
-                  <th>Temperature</th>
-                  <th>Humidity</th>
-                  <th>Date-time</th>
-                </tr>
-              </thead>
-              <tbody>
-                {climates?.map((climate) => (
-                  <tr key={climate.id}>
-                    <td className='text-red-400 font-medium'>
-                      {climate.temperature}&deg;C
-                    </td>
-                    <td className='text-blue-400 font-medium'>
-                      {climate.humidity}%
-                    </td>
-                    <td>
-                      {format(
-                        new Date(climate.created_at),
-                        'MMM dd, yyyy hh:mm a',
-                      )}
-                    </td>
+          <div className='card shadow-lg col-span-2 flex flex-col row-span-3'>
+            <CSVLink
+              className='btn btn-primary self-end mb-2 btn-sm'
+              data={csvData}
+              filename={`${format(new Date(), 'MM-dd-yyyy')}_climate.csv`}>
+              Download CSV
+            </CSVLink>
+            <div className='flex-grow card-body h-0 overflow-y-auto'>
+              <table className='table'>
+                <thead>
+                  <tr>
+                    <th>Temperature</th>
+                    <th>Humidity</th>
+                    <th>Date-time</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {sortedDataForTable?.map((climate) => (
+                    <tr key={climate.id}>
+                      <td className='text-red-400 font-medium'>
+                        {climate.temperature}&deg;C
+                      </td>
+                      <td className='text-blue-400 font-medium'>
+                        {climate.humidity}%
+                      </td>
+                      <td>
+                        {format(
+                          new Date(climate.created_at),
+                          'MMM dd, yyyy hh:mm a',
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
-      </div>
+      ) : isLoading ? (
+        <div className='flex items-center justify-center w-full h-full'>
+          <Loader2 className='h-8 w-8 animate-spin' />
+        </div>
+      ) : null}
     </div>
   );
 }
